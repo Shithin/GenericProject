5f88c53b0ef0a0e50e66dd29fa7f05e7e0e4f5a9
Shithin_Focaloid <shithin@focaloid.com>
2018-10-05 13:12:37 +0530

    Initial Commit

---
+++ GenericProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericProject
+//
+//  Created by Shithin_Focaloid on 05/10/18.
+//  Copyright © 2018 E3Help. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
